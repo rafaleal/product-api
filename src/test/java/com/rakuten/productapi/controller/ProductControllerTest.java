965486bdede92bfa3cc277944f9547e994cf4849
Rafael Leal <leal.rafa22@gmail.com>
2019-06-03 05:21:15 -0300

    Create shell of ProductControllerTest

---
+++ src/test/java/com/rakuten/productapi/controller/ProductControllerTest.java
@@ -0,0 +1,12 @@
+package com.rakuten.productapi.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@WebMvcTest
+class ProductControllerTest {
+
+}
