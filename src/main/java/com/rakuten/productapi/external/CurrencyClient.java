92ac4b7868b75b8ea94a1357a6478422fa99b1a1
Rafael Leal <leal.rafa22@gmail.com>
2019-06-03 05:03:56 -0300

    Create Feign unit test

--- src/main/java/com/rakuten/productapi/external/CurrencyClient.java
+++ src/main/java/com/rakuten/productapi/external/CurrencyClient.java
@@ -2,13 +2,14 @@ package com.rakuten.productapi.external;
 
 import com.rakuten.productapi.dto.QuoteDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import static com.rakuten.productapi.constant.ExternalEndpoints.API_CURRENCY_LATEST;
-
 @FeignClient(name="${feign.name}", url = "${feign.url}")
 public interface CurrencyClient {
-    @GetMapping(API_CURRENCY_LATEST)
-    QuoteDTO getCurrency(@RequestParam String base, @RequestParam String symbols);
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    QuoteDTO getCurrency(@RequestParam(value = "base") String base, @RequestParam(value = "symbols") String symbols);
+
 }
