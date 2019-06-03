0e6ab33dd5beea097c1f428e8a1fbe2b3730bbbf
Rafael Leal <leal.rafa22@gmail.com>
2019-06-03 05:37:17 -0300

    Update the variables to uppercase

--- src/main/java/com/rakuten/productapi/config/SwaggerConfig.java
+++ src/main/java/com/rakuten/productapi/config/SwaggerConfig.java
@@ -19,13 +19,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
     private static final String SWAGGER_API_VERSION = "1.0";
     private static final String LICENSE_TEXT = "License";
-    private static final String title = "Products REST API";
-    private static final String description = "RESTful API for Rakuten Products";
+    private static final String TITLE = "Products REST API";
+    private static final String DESCRIPTION = "RESTful API for Rakuten Products";
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title(title)
-                .description(description)
+                .title(TITLE)
+                .description(DESCRIPTION)
                 .license(LICENSE_TEXT)
                 .version(SWAGGER_API_VERSION)
                 .build();
