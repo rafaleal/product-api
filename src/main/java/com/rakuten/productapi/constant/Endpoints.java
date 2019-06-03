eeb2825a0cdc2d64c4f521f3cbbda95c71f5fcf7
Rafael Leal <leal.rafa22@gmail.com>
2019-06-03 05:37:35 -0300

    Create private constructor

--- src/main/java/com/rakuten/productapi/constant/Endpoints.java
+++ src/main/java/com/rakuten/productapi/constant/Endpoints.java
@@ -5,4 +5,6 @@ public class Endpoints {
     public static final String API_V1 = "/api/v1";
     public static final String API_PRODUCTS = "/products";
     public static final String API_PRODUCTS_ID = "/{uuid}";
+
+    private Endpoints() {}
 }
