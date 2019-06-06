22697acf8de317e13f47d24144ae23d1f15aa8d8
Rafael Leal <leal.rafa22@gmail.com>
2019-06-06 06:35:11 -0300

    Create new service methods

--- src/main/java/com/rakuten/productapi/service/ProductService.java
+++ src/main/java/com/rakuten/productapi/service/ProductService.java
@@ -2,12 +2,19 @@ package com.rakuten.productapi.service;
 
 import com.rakuten.productapi.domain.Product;
 import com.rakuten.productapi.dto.ProductDTO;
+import com.rakuten.productapi.exception.ProductBadRequestException;
+import com.rakuten.productapi.exception.ProductNotFoundException;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface ProductService {
-    Product createProduct(ProductDTO product);
-    void deleteProduct(UUID uuid);
-    Product updateProduct(UUID uuid, List<String> categories);
+    Product getProduct(UUID id) throws ProductNotFoundException;
+
+    List<Product> getAllProducts();
+
+    Product createProduct(ProductDTO product) throws ProductBadRequestException;
+    void deleteProduct(UUID id) throws ProductNotFoundException;
+    Product addCategoryToProduct(UUID productId, String category) throws ProductNotFoundException;
+    Product removeCategoryFromProduct(UUID uuid, String category) throws Exception;
 }
--- src/main/java/com/rakuten/productapi/service/ProductServiceImpl.java
+++ src/main/java/com/rakuten/productapi/service/ProductServiceImpl.java
@@ -4,11 +4,16 @@ import com.rakuten.productapi.domain.Money;
 import com.rakuten.productapi.domain.Product;
 import com.rakuten.productapi.dto.ProductDTO;
 import com.rakuten.productapi.dto.mapper.ProductMapper;
+import com.rakuten.productapi.exception.ProductBadRequestException;
+import com.rakuten.productapi.exception.ProductNotFoundException;
 import com.rakuten.productapi.repository.ProductMongoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -19,29 +24,77 @@ public class ProductServiceImpl implements ProductService{
     ProductMapper productMapper;
 
     @Autowired
-    public ProductServiceImpl(ProductMongoRepository repository, ExchangeService exchangeService, ProductMapper productMapper) {
-        this.productMongoRepository = repository;
+    public ProductServiceImpl(ProductMongoRepository productMongoRepository, ExchangeService exchangeService, ProductMapper productMapper) {
+        this.productMongoRepository = productMongoRepository;
         this.exchangeService = exchangeService;
         this.productMapper = productMapper;
     }
 
     @Override
-    public Product createProduct(ProductDTO productDTO) {
-        Product product = productMapper.mapProductDtoToProduct(productDTO);
-        Money exchangedMoney = this.exchangeService.exchangeAmount(product.getMoney());
-        product.setMoney(exchangedMoney);
-        return productMongoRepository.save(product);
+    public Product getProduct(UUID id) throws ProductNotFoundException {
+        Optional<Product> optionalProduct = productMongoRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            return optionalProduct.get();
+        } else {
+            throw new ProductNotFoundException();
+        }
     }
 
     @Override
-    public void deleteProduct(UUID uuid) {
-
+    public List<Product> getAllProducts() {
+        return productMongoRepository.findAll();
     }
 
     @Override
-    public Product updateProduct(UUID uuid, List<String> categories) {
-        return null;
+    public Product createProduct(ProductDTO productDTO) throws ProductBadRequestException {
+        if (productDTO.getMoney().getAmount().compareTo(BigDecimal.ZERO) > 0) {
+            Product product = productMapper.mapProductDtoToProduct(productDTO);
+            product.setCreatedAt(LocalDate.now());
+            Money exchangedMoney = this.exchangeService.exchangeAmount(product.getMoney());
+            product.setMoney(exchangedMoney);
+            product.setId(UUID.randomUUID());
+
+
+            return productMongoRepository.save(product);
+        } else {
+            throw new ProductBadRequestException();
+        }
     }
 
+    @Override
+    public void deleteProduct(UUID id) throws ProductNotFoundException {
+        if (productMongoRepository.existsById(id)) {
+            productMongoRepository.deleteById(id);
+        } else {
+            throw new ProductNotFoundException();
+        }
+    }
+
+    @Override
+    public Product addCategoryToProduct(UUID productId, String category) throws ProductNotFoundException {
+        Optional<Product> optionalProduct = productMongoRepository.findById(productId);
+        if (optionalProduct.isPresent()) {
+            optionalProduct.get().getCategories().add(category);
+            return productMongoRepository.save(optionalProduct.get());
+        } else {
+            throw new ProductNotFoundException();
+        }
+    }
+
+    @Override
+    public Product removeCategoryFromProduct(UUID uuid, String category) throws Exception {
+
+        Optional<Product> optionalProduct = productMongoRepository.findById(uuid);
+        if (optionalProduct.isPresent()) {
+            boolean wasRemoved = optionalProduct.get().getCategories().remove(category);
+
+            if(wasRemoved) {
+                return productMongoRepository.save(optionalProduct.get());
+            }
+            throw new ProductBadRequestException();
+        } else {
+            throw new ProductNotFoundException();
+        }
+    }
 
 }
