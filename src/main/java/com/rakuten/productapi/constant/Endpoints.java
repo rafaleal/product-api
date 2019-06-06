c1e9f23fca5874892dacf3f0f81f0c556db5a097
Rafael Leal <leal.rafa22@gmail.com>
2019-06-06 06:32:48 -0300

    Rename endpoints

--- src/main/java/com/rakuten/productapi/constant/Endpoints.java
+++ src/main/java/com/rakuten/productapi/constant/Endpoints.java
@@ -5,6 +5,8 @@ public class Endpoints {
     public static final String API_V1 = "/api/v1";
     public static final String API_PRODUCTS = "/products";
     public static final String API_PRODUCTS_ID = "/{uuid}";
+    public static final String API_ASSIGN_CATEGORIES = "/assign-category";
+    public static final String API_REMOVE_CATEGORIES = "/remove-category";
 
     private Endpoints() {}
 }
